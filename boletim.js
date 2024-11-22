9b811d58ef45777b7e2e65d9af49ac1b77690859
Priscila <pri.wagner.silva@gmail.com>
2024-11-22 18:09:23 +0000

    add footer

--- boletim.js
+++ boletim.js
@@ -4,7 +4,7 @@ const imgReprovado = '<img src="./images/reprovado.png" alt="emoji decepcionada"
 const atividades = [];
 const notas = [];
 const spanAprovado = '<span class="resultado aprovado">Aprovado</span>';
-const spanReprovado = '<span class="resultado reprovado>Reprovado</span>';
+const spanReprovado = '<span class="resultado reprovado">Reprovado</span>';
 const notaMinima = parseFloat(prompt("Digite a nota mínima:"));
 
 let linhas = '';
@@ -25,7 +25,7 @@ function adicionaLinha() {
         alert (`A atividade: $(inputNomeatividade.value} já foi inserida`);
     } else {
     atividades.push(inputNomeAtividade.value);
-    notas.pus(parseFloat(inputNotaAtividade.value));
+    notas.push(parseFloat(inputNotaAtividade.value));
 
     let linha = '<tr>';
     linha += `<td>${inputNomeAtividade.value}</td>`;
@@ -36,6 +36,11 @@ function adicionaLinha() {
     linhas += linha;
     }
 
+    function atualizaTabela() {
+        const corpoTabela = document.querySelector('tbody');
+        corpoTabela.innerHTML = linhas;
+    }
+
     inputNomeAtividade.value = '';
     inputNotaAtividade.value = '';
 }
@@ -43,7 +48,13 @@ function adicionaLinha() {
 function atualizaMediaFinal() {
 
     const mediaFinal = calculaMediaFinal();
-    let somaDasNotas =0;
+    document.getElementById('media-final-valor').innerHTML =mediaFinal;
+    document.getElementById('media-final-resultado').innerHTML = mediaFinal >= 7 ? 'Aprovado' : 'Reprovado';
+}
+
+
+    function calculaMediaFinal() {
+        let somaDasNotas = 0;
 
     for (let i=0; i < notas.length; i++) {
         somaDasNotas += notas[i];
