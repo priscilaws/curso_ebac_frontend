5cf9326fe01f797fe872d2ecf11d9f96e2e5f74e
Priscila <pri.wagner.silva@gmail.com>
2025-04-14 16:02:08 +0100

    added arrow function

---
+++ src/notas-boletim.js
@@ -0,0 +1,18 @@
+const estudantes = [
+    { nome: "Debora", nota: 7.5 },
+    { nome: "Joao", nota: 5.0 },
+    { nome: "Maria", nota: 8.0 },
+    { nome: "Carolina", nota: 6.5 },
+    { nome: "Lucas", nota: 3.0 }
+];
+
+const filtraAprovados = lista => lista.filter(estudantes => estudantes.nota >= 6)
+const filtraReprovados = lista => lista.filter(estudantes => estudantes.nota < 6)
+
+const alunosAprovados = filtraAprovados(estudantes);
+console.log("Alunos provados:");
+console.log(alunosAprovados);
+
+const alunosReprovados = filtraReprovados(estudantes);
+console.log("Alunos reprovados:");
+console.log(alunosReprovados);
