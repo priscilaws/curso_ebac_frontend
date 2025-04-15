a2288c229ab08c46a4e57b21e1665b4d96119f03
Priscila <pri.wagner.silva@gmail.com>
2025-04-15 17:34:19 +0100

    added typescript

---
+++ dist/teste.js
@@ -0,0 +1,11 @@
+"use strict";
+function multiplicar(a, b) {
+    return a * b;
+}
+function dizOi(nome) {
+    return "Oi" + nome + "!";
+}
+const resultado = multiplicar(3, 20);
+console.log(resultado);
+const saudar = dizOi("John");
+console.log(saudar);
---
+++ src/teste.ts
@@ -0,0 +1,13 @@
+function multiplicar(a: number, b: number): number {
+    return a * b;
+}
+
+function dizOi(nome: string): string {
+    return "Oi" + nome + "!";
+}
+
+const resultado = multiplicar(3, 20);
+console.log(resultado);
+
+const saudar = dizOi("John");
+console.log(saudar);
