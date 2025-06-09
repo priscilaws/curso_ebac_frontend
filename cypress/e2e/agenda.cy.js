c3b21f466fbac56efdc212a66e36e42e38355e7e
Priscila <pri.wagner.silva@gmail.com>
2025-06-09 17:06:04 +0100

    teste criado

--- cypress/e2e/agenda.cy.js
+++ cypress/e2e/agenda.cy.js
@@ -1,7 +1,9 @@
 describe('Testes da Agenda de Contatos', () => {
-    const nome = 'Teste Cypress';
+    const nome = 'Pedro Silva';
     const telefone = '11999999999';
-    const nomeEditado = 'Teste Cypress Editado';
+    const email = 'teste@exemplo.com';
+    const nomeEditado = 'Pedro Silva Editado';
+    const emailEditado = 'pedrosilva@exemplo.com';
 
     beforeEach(() => {
     cy.visit('https://agenda-contatos-react.vercel.app/');
@@ -11,36 +13,51 @@ describe('Testes da Agenda de Contatos', () => {
     it('Deve adicionar um novo contato', () => {
     cy.get('input[placeholder="Nome"]').type(nome);
     cy.get('input[placeholder="Telefone"]').type(telefone);
+    cy.get('input[placeholder="E-mail"]').type(email);
     cy.contains('Adicionar').click();
+
     cy.contains(nome).should('exist');
     cy.contains(telefone).should('exist');
+    cy.contains(email).should('exist');
     });
 
-    it('Deve editar um contato existente', () => {
+    it.only('Deve editar um contato existente', () => {
+
     cy.get('input[placeholder="Nome"]').type(nome);
     cy.get('input[placeholder="Telefone"]').type(telefone);
+    cy.get('input[placeholder="E-mail"]').type(email);
     cy.contains('Adicionar').click();
 
-    cy.contains(nome).parent().within(() => {
-        cy.contains('Editar').click();
+
+    cy.contains(nome).parents('div').parents('div').first().within(() => {
+        cy.get('button.edit').click();
     });
 
+
     cy.get('input[placeholder="Nome"]').clear().type(nomeEditado);
+    cy.get('input[placeholder="E-mail"]').clear().type(emailEditado);
     cy.contains('Salvar').click();
 
+
     cy.contains(nomeEditado).should('exist');
-    cy.contains(nome).should('not.exist');
+    cy.contains(emailEditado).should('exist');
+    cy.contains(email).should('not.exist');
     });
 
     it('Deve remover um contato', () => {
+
     cy.get('input[placeholder="Nome"]').type(nome);
     cy.get('input[placeholder="Telefone"]').type(telefone);
+    cy.get('input[placeholder="E-mail"]').type(email);
     cy.contains('Adicionar').click();
 
-    cy.contains(nome).parent().within(() => {
-        cy.contains('Excluir').click();
+
+    cy.contains(nome).parents('div').first().within(() => {
+        cy.get('button.delete').click();
     });
 
+
     cy.contains(nome).should('not.exist');
+    cy.contains(email).should('not.exist');
     });
 });
